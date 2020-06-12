d4b9d948d163a86148dc33943f0492016991a0f2
Carlos Ingles <Carlos.Ingles@team.telstra.com>
2020-06-12 19:50:12 +1000

    feat: init project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal website
+
+Project uses [vuepress](!https://vuepress.vuejs.org/).
+
+`npm run serve` to serve
+`npm run build` to build
---
+++ src/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Carlos | Software Developer',
+  description: 'Carlos Ingles is a software developer based in Melbourne',
+  dest: '../dist'
+}
